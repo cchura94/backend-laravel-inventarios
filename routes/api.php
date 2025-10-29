c68b474517e588de7c35de9bfaa9932cc288dc75
cristian <cchura.cpc@gmail.com>
2025-10-28 20:48:05 -0400

    middlewares en CRUD usuarios

--- routes/api.php
+++ routes/api.php
@@ -10,22 +10,25 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-// CRUD Usuarios
-Route::get("/usuario", [UsuarioController::class, "funListar"]);
-Route::post("/usuario", [UsuarioController::class, "funGuardar"]);
-Route::get("/usuario/{id}", [UsuarioController::class, "funMostrar"]);
-Route::put("/usuario/{id}", [UsuarioController::class, "funModificar"]);
-Route::delete("/usuario/{id}", [UsuarioController::class, "funEliminar"]);
 
 // Auth
 Route::prefix('/v1/auth')->group(function(){
 
     Route::post("/login", [AuthController::class, "funLogin"]);
     Route::post("/register", [AuthController::class, "funRegister"]);
-
+    
     Route::middleware('auth:sanctum')->group(function(){
         Route::get("/profile", [AuthController::class, "funProfile"]);
         Route::post("/logout", [AuthController::class, "funLogout"]);
     });
 });
 
+Route::middleware('auth:sanctum')->group(function(){
+    // CRUD Usuarios
+    Route::get("/usuario", [UsuarioController::class, "funListar"]);
+    Route::post("/usuario", [UsuarioController::class, "funGuardar"]);
+    Route::get("/usuario/{id}", [UsuarioController::class, "funMostrar"]);
+    Route::put("/usuario/{id}", [UsuarioController::class, "funModificar"]);
+    Route::delete("/usuario/{id}", [UsuarioController::class, "funEliminar"]);
+
+});
