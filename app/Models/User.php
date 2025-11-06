cb05b793d49d0459b100529ee7152a456e4d54c4
cristian <cchura.cpc@gmail.com>
2025-11-05 20:24:43 -0400

    Relacion M:N muchos a muchos

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     public function persona(){
         return $this->hasOne(Persona::class);
     }
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
 }
